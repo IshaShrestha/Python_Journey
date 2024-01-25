28721b177fc6979e3a3d51a26cea9702f83f2276
Isha Shrestha <eeshashrestha18@gmail.com>
2024-01-25 14:43:14 +0545

    Chapter1 completed

---
+++ Chapter1/challange1.py
@@ -0,0 +1,4 @@
+#Write and save a program that prints out your name and waits for the user to press the Enter key before the program ends. Then, run the program by double-clicking its icon.
+
+print("Hello I am Isha")
+input("/n/nPress enter to exit")
---
+++ Chapter1/challange2.py
@@ -0,0 +1,4 @@
+ #Write a program that prints your favorite quote. It should give credit to the person who said it on the next line.\
+
+print("Its not the end of the story")
+print("            -Stephen Hawking")
